e919aada91aa104083cc1b00063fb4c2fee161be
chrismdks <chris.mdks@gmail.com>
2023-04-22 19:33:27 +0300

    add color to text when clicking it with NavLink

--- src/Header.jsx
+++ src/Header.jsx
@@ -20,13 +20,23 @@ function Header() {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                     <li className="nav-item">
-                        <NavLink className="nav-link" aria-current="page" to="/">Home</NavLink>
+                        <NavLink 
+                            className={ ({isActive}) => isActive? "nav-link active text-info" : "nav-link" }
+                            aria-current="page" 
+                            to="/"
+                        >Home</NavLink>
                     </li>
                     <li className="nav-item">
-                        <NavLink className="nav-link" to="/about">About</NavLink>
+                        <NavLink 
+                            className={ ({isActive}) => isActive? "nav-link active text-info" : "nav-link" }
+                            to="/about"
+                        >About</NavLink>
                     </li>
                     <li className="nav-item">
-                        <NavLink className="nav-link" to="/cryptodetail/BTC">Crypto Detail</NavLink>
+                        <NavLink 
+                            className={ ({isActive}) => isActive? "nav-link active text-info" : "nav-link" } 
+                            to="/cryptodetail/BTC"
+                        >Crypto Detail</NavLink>
                     </li>
                     <li className="nav-item dropdown">
                         <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
