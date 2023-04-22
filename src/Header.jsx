dbcc2255e296412d9256295cd16e85c2b181ce8d
chrismdks <chris.mdks@gmail.com>
2023-04-22 19:23:44 +0300

    use NavLink instead of Link to highlight the currently active link

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,7 @@
 import logo from './Images/logo192.png'
 //type rfce and enter (from extension ES7+)
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 function Header() {
   return (
@@ -20,13 +20,13 @@ function Header() {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                     <li className="nav-item">
-                        <Link className="nav-link active" aria-current="page" to="/">Home</Link>
+                        <NavLink className="nav-link" aria-current="page" to="/">Home</NavLink>
                     </li>
                     <li className="nav-item">
-                        <Link className="nav-link" to="/about">About</Link>
+                        <NavLink className="nav-link" to="/about">About</NavLink>
                     </li>
                     <li className="nav-item">
-                        <Link className="nav-link" to="/cryptodetail/BTC">Crypto Detail</Link>
+                        <NavLink className="nav-link" to="/cryptodetail/BTC">Crypto Detail</NavLink>
                     </li>
                     <li className="nav-item dropdown">
                         <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
