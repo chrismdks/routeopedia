e3b1c802c2b899a26729fcd5c57d9ede026c49e9
chrismdks <chris.mdks@gmail.com>
2023-04-26 15:58:55 +0300

    3 different ways to navigate: 1.useNavigate, 2.Link component, 3.useState & Navigate component

--- src/Pages/Product/Product.jsx
+++ src/Pages/Product/Product.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import { useNavigate, Link, Navigate } from 'react-router-dom'
+import { useState } from 'react';
 
 function Product() {
+
+  const navigate = useNavigate();
+
+  const [goToProduct, setGoToProduct] = useState(() => {
+    return false;
+  })
+
   return (
-    <div>Product</div>
+    <div> Product
+      
+      {/* Navigate using useNavigate */}
+      <button onClick={ () => {navigate("/product/create")} }>Add Product (useNavigate)</button>
+
+      {/* Navigate using <Link> component */}
+      <Link to={'/product/details/005'} >
+        <button>Product detail - 005 (Link)</button>
+      </Link>
+
+      {/* Navigate using useState & <Navigate> component */}
+      { goToProduct && <Navigate to="/product/details/003"></Navigate> }
+      <button onClick={ () => {
+        setGoToProduct( {goToProduct:true} ) } 
+      }>Product detail - 003 (useState)</button>
+    </div>
   )
 }
 
